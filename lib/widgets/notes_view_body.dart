9c6cb9eb77d0025c5d448f89348b7aec1264d2b9
ahmedelne7 <ahmedelnahrawy881@gmail.com>
2024-12-05 15:38:43 +0200

    Adding note item

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/widgets/custom_app_bar.dart';
+import 'package:note_app/widgets/note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -12,9 +13,10 @@ class NotesViewBody extends StatelessWidget {
         child: Column(
           children: [
             SizedBox(
-              height: 50,
+              height: 40,
             ),
-            CustomAppBar()
+            CustomAppBar(),
+            NoteItem()
           ],
         ),
       ),
