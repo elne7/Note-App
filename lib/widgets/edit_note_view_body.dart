dbadd878470725dbe7ad10f166489a9efe59a08e
ahmedelne7 <ahmedelnahrawy881@gmail.com>
2024-12-11 09:10:29 +0200

    update custom app bar

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_app_bar.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            CustomAppBar(
+              title: 'Edit Note',
+              icon: Icons.check,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
