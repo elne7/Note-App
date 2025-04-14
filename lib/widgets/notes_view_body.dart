1fb81a45db1cde0974d268e411de764458d3c020
Ahmed Elnahrawy <ahmedelnahrawy881@gmail.com>
2025-04-15 01:31:50 +0200

    update add note button sheet ui

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -12,6 +12,7 @@ class NotesViewBody extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
+            isScrollControlled: true,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(16),
             ),
